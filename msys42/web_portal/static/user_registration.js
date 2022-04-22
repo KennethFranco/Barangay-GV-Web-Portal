bec8b896f98208e97ad9a849aea8d4fe83d3fc5c
MarcoFrancisco13 <franciscomarco2921@gmail.com>
2022-04-22 20:58:39 +0800

    EOD April 22

--- msys42/web_portal/static/user_registration.js
+++ msys42/web_portal/static/user_registration.js
@@ -19,139 +19,38 @@ var account_infoPage = document.getElementById('account_info')
 var addressPage = document.getElementById('addressPage')
 var submitPage = document.getElementById('submitPage')
 
-var personal_detailsHTML = `
-    <div class="row">
-        <div class="col-lg-6 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="last_name">Last Name<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="last_name" name="last_name" class="bodyFont formInput" type="text"
-                placeholder="Last Name">
-        </div>
-        <div class="col-lg-4 col-sm-6 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="nationality">Nationality<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="nationality" name="nationality" class="bodyFont formInput" type="text"
-                placeholder="Nationality">
-        </div>
-        <div class="col-lg-2 col-sm-6 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="sex">Sex<span
-                    style="color: #B30000;">*</span></label><br>
-            <select id="sex" name="sex" class="bodyFont formInput" type="text" placeholder="Sex">
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-                <option value="Prefer not to say">Prefer not to say</option>
-            </select>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-6 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="first_name">First Name<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="first_name" name="first_name" class="bodyFont formInput" type="text"
-                placeholder="Last Name">
-        </div>
-        <div class="col-lg-3 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="civil_status">Civil Status<span
-                    style="color: #B30000;">*</span></label><br>
-            <select id="civil_status" name="civil_status" class="bodyFont formInput" type="text"
-                placeholder="Civil Status">
-                <option value="Single">Single</option>
-                <option value="Married">Married</option>
-                <option value="Divorced">Divorced</option>
-                <option value="Widowed">Widowed</option>
-            </select>
-        </div>
-        <div class="col-lg-3 col-sm-6 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="age">Age<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="age" name="age" class="bodyFont formInput" type="number" placeholder="Age">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-6 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="middle_name">Middle Name<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="middle_name" name="middle_name" class="bodyFont formInput" type="text"
-                placeholder="Middle Name">
-        </div>
-        <div class="col-lg-3 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="birthday">Birthday<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="birthday" name="birthday" class="bodyFont formInput" type="date">
-        </div>
-        <div class="col-lg-3 mb-4">
+// FORM ELEMENTS
+var last_name = document.getElementById('last_name')
+var first_name = document.getElementById('first_name')
+var middle_name = document.getElementById('middle_name')
+var nationality = document.getElementById('nationality')
+var sex = document.getElementById('sex')
+var civil_status = document.getElementById('civil_status')
+var age = document.getElementById('age')
+var birthday = document.getElementById('birthday')
 
-        </div>
-    </div>    
-        `
-var account_infoHTML = `
-    <div class="row">
-        <div class="col-lg-6 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="username">Username<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="username" name="username" class="bodyFont formInput" type="text"
-                placeholder="Username">
-        </div>
-        <div class="col-lg-6 spacer">
-            
-        </div>
-        </div>
-        <div class="row">
-        <div class="col-lg-6 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="email">Email<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="email" name="email" class="bodyFont formInput" type="text"
-                placeholder="Email">
-        </div>
-        <div class="col-lg-6 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="confirm_email">Confirm Email<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="confirm_email" name="confirm_email" class="bodyFont formInput" type="text"
-                placeholder="Confirm Email">
-        </div>
-        </div>
-        <div class="row">
-        <div class="col-lg-6 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="contact_number">Contact Number<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="contact_number" name="contact_number" class="bodyFont formInput" type="text"
-                placeholder="9XXXXXXXXX">
-        </div>
-        <div class="col-lg-6 spacer">
-        </div>
-        </div>
-        <div class="row">
-        <div class="col-lg-6 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="password">Password<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="password" name="password" class="bodyFont formInput" type="text"
-                placeholder="Password">
-        </div>
-        <div class="col-lg-6 col-sm-12 mb-4">
-            <label class="bodyFont formLabel globalSubHeader" for="confirm_password">Confirm Password<span
-                    style="color: #B30000;">*</span></label><br>
-            <input id="confirm_password" name="confirm_password" class="bodyFont formInput" type="text"
-                placeholder="Confirm Password">
-        </div>
-    </div>
-        `
-var addressHTML = `
-        WOAH WOAH WAOH
-        
-        `
-var submitHTML = `
-        FUCK 
-        `
+var username = document.getElementById('username')
+var contact_number = document.getElementById('contact_number')
+var email = document.getElementById('email')
+var confirm_email = document.getElementById('confirm_email')
+var password = document.getElementById('password')
+var confirm_password = document.getElementById('confirm_password')
 
-personal_detailsPage.innerHTML = personal_detailsHTML
-account_infoPage.innerHTML = account_infoHTML
-addressPage.innerHTML += addressHTML
-submitPage.innerHTML += submitHTML
+var street = document.getElementById('street')
+var province = document.getElementById('province')
+var city = document.getElementById('city')
+var barangay = document.getElementById('barangay')
+var zip_code = document.getElementById('zip_code')
 
 personal_detailsPage.style.display = "block"
 account_infoPage.style.display = "none"
 addressPage.style.display = "none"
 submitPage.style.display = "none"
+
+pd_isIncomplete = false;
+ai_isIncomplete = false;
+add_isIncomeplete = false;
+var errorMessage = ""
 
 // BUTTON LOGIC
 // Go to ACCOUNT INFO
@@ -172,7 +71,9 @@ goToAccountInfo.addEventListener("click", function () {
     submitPage.style.display = "none"
 
 
-    console.log("GO TO ACCOUNT INFO")
+    // if () {
+
+    // }
 });
 
 // Back to PERSONAL DETAILS
